8e8f9eb337fda00b43c9798fe07bcff8622e63f8
root <root@controller.fritz.box>
2019-08-04 15:05:43 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"alarmserver adapter settings": {
+		"en": "Adapter settings for alarmserver",
+		"de": "Adaptereinstellungen für alarmserver",
+		"ru": "Настройки адаптера для alarmserver",
+		"pt": "Configurações do adaptador para alarmserver",
+		"nl": "Adapterinstellingen voor alarmserver",
+		"fr": "Paramètres d'adaptateur pour alarmserver",
+		"it": "Impostazioni dell'adattatore per alarmserver",
+		"es": "Ajustes del adaptador para alarmserver",
+		"pl": "Ustawienia adaptera dla alarmserver",
+		"zh-cn": "alarmserver的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
